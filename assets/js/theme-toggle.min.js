ed08d8f1027ac455e5ef724f89ff81457be9d4a4
GitHub <noreply@github.com>
2024-12-05 20:55:58 +0000

    Continuous Integration Publish: 2f4789c380bb0b08542b5d7571974ab55eb364e3 Merge pull request #94 from alexaveldanez/choco-theme-0.8.2
    (#93) Upgrade to choco-theme 0.8.2

--- assets/js/theme-toggle.min.js
+++ assets/js/theme-toggle.min.js
@@ -1,5 +1,5 @@
 /*!
-  * choco-theme v0.8.1 (https://github.com/chocolatey/choco-theme#readme)
+  * choco-theme v0.8.2 (https://github.com/chocolatey/choco-theme#readme)
   * Copyright 2020-2024 Chocolatey Software
   * Licensed under MIT (https://github.com/chocolatey/choco-theme/blob/main/LICENSE)
 */
